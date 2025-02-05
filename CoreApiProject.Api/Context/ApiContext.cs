4ff09f3aa8b39e72db2dd2ec3883949f6c5fcf35
Orçun Aşık <asikorcun@gmail.com>
2025-02-05 13:55:46 +0300

    Created ApiContext class

---
+++ CoreApiProject.Api/Context/ApiContext.cs
@@ -0,0 +1,22 @@
+﻿using CoreApiProject.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApiProject.Api.Context;
+public class ApiContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=127.0.0.1;Initial Catalog=CoreApiProject;User ID=sa;Password=Password1;");
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Chef> Chefs { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+}
