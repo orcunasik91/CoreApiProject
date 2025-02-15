cd192178d0f31ffc8b40162a77ec7975daee3d01
Orçun Aşık <asikorcun@gmail.com>
2025-02-15 23:31:52 +0300

    Service registrations for dbcontext, automapper

--- CoreApiProject.Api/Program.cs
+++ CoreApiProject.Api/Program.cs
@@ -1,8 +1,13 @@
+using CoreApiProject.Api.Context;
+using System.Reflection;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiContext>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 WebApplication app = builder.Build();
 
