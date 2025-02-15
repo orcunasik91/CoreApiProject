f43513f03f06f793c6b2dd4284039f0a2842c923
Orçun Aşık <asikorcun@gmail.com>
2025-02-15 23:29:45 +0300

    Created category dto classes

---
+++ CoreApiProject.Api/Dtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿using CoreApiProject.Api.Entities;
+
+namespace CoreApiProject.Api.Dtos.CategoryDtos;
+public class CreateCategoryDto
+{
+    public string CategoryName { get; set; }
+}
---
+++ CoreApiProject.Api/Dtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿using CoreApiProject.Api.Entities;
+
+namespace CoreApiProject.Api.Dtos.CategoryDtos;
+public class UpdateCategoryDto
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
