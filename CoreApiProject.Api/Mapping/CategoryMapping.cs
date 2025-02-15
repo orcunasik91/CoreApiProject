2bfbfb01effd9fd30b79739bb965fe93d79dffc7
Orçun Aşık <asikorcun@gmail.com>
2025-02-15 23:30:05 +0300

    mapping category

---
+++ CoreApiProject.Api/Mapping/CategoryMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CoreApiProject.Api.Dtos.CategoryDtos;
+using CoreApiProject.Api.Entities;
+
+namespace CoreApiProject.Api.Mapping;
+public class CategoryMapping : Profile
+{
+    public CategoryMapping()
+    {
+        CreateMap<CreateCategoryDto, Category>().ReverseMap();
+        CreateMap<ResultCategoryDto, Category>().ReverseMap();
+        CreateMap<UpdateCategoryDto, Category>().ReverseMap();
+    }
+}
