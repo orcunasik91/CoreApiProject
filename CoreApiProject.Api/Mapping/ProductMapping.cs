da797865dcf2f767cabe8b3f573907ef9f968a91
Orçun Aşık <asikorcun@gmail.com>
2025-03-09 23:29:01 +0300

    Created ProductMapping

---
+++ CoreApiProject.Api/Mapping/ProductMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoreApiProject.Api.Dtos.ProductDtos;
+using CoreApiProject.Api.Entities;
+
+namespace CoreApiProject.Api.Mapping;
+public class ProductMapping : Profile
+{
+    public ProductMapping()
+    {
+        CreateMap<CreateProductDto, Product>().ReverseMap();
+        CreateMap<ResultProductDto, Product>().ReverseMap();
+        CreateMap<UpdateProductDto, Product>().ReverseMap();
+        CreateMap<GetByIdProductDto, Product>().ReverseMap();
+    }
+}
