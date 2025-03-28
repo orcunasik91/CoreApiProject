0232fa78b933d7a9c9f3966299158bc2d9ee2255
Orçun Aşık <asikorcun@gmail.com>
2025-03-28 19:30:57 +0300

    added template for webUI

---
+++ CoreApiProject.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreApiProject.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
