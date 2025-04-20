25620781198da473fbcd21329d51f4bd10e7d726
Orçun Aşık <asikorcun@gmail.com>
2025-04-20 16:53:31 +0300

    removed

--- CoreApiProject.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreApiProject.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
