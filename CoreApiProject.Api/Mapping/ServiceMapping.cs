21274ccea2356edcfccee775dcd13ddb38184f7f
Orçun Aşık <asikorcun@gmail.com>
2025-04-20 16:54:59 +0300

    Added service mapping

---
+++ CoreApiProject.Api/Mapping/ServiceMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoreApiProject.Api.Dtos.ServiceDtos;
+using CoreApiProject.Api.Entities;
+
+namespace CoreApiProject.Api.Mapping;
+public class ServiceMapping : Profile
+{
+    public ServiceMapping()
+    {
+        CreateMap<CreateServiceDto, Service>().ReverseMap();
+        CreateMap<ResultServiceDto, Service>().ReverseMap();
+        CreateMap<UpdateServiceDto, Service>().ReverseMap();
+        CreateMap<GetByIdServiceDto, Service>().ReverseMap();
+    }
+}
