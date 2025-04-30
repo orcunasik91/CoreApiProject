9a7c8a8a17e1d6fdf7b4de7fb8a2279075edee7b
Orçun Aşık <asikorcun@gmail.com>
2025-04-30 20:07:02 +0300

    Created productdtos for webUI

---
+++ CoreApiProject.UI/Dtos/ProductDtos/ResultProductsWithCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreApiProject.UI.Dtos.ProductDtos;
+public class ResultProductsWithCategoryDto
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string CategoryName { get; set; }
+}
